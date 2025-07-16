edd2b679c71bbbcccd2b6d3e3ddb99a555726fba
Nazife Nur Temiz <nnurtmz52@gmail.com>
2025-07-16 10:29:21 +0300

    initial commit the 2nd

---
+++ Restaurant-Order-System/src/test/java/com/nazifr/RestaurantOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazifr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
